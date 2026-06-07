523c11fcb2f8e05b89931e09b1b11e8aa0959d24
pedrofcruz <pedrofcrux@gmail.com>
2026-06-07 20:31:01 -0300

    monitorar emergencia

--- central/main_central.py
+++ central/main_central.py
@@ -9,7 +9,14 @@ class Servidor_Central(Servidor):
     def __init__(self):
         super().__init__()
         self.ser = setup()
-    
+        self.t_emergencia = threading.Thread(
+            target=self.monitora_emergencia,
+            args=(),
+            daemon=True
+        )
+        self.t_emergencia.start()
+
+
     def handle_multa(self, data):
         print('-='*25)
         print("multa")
@@ -95,16 +102,54 @@ class Servidor_Central(Servidor):
 
         return retorno
 
+    def monitora_emergencia(self):
+        emergencia = False
+        while self.rodando:
+            isOk, _ = modbus_enviar_comando(self.ser, 0x20, 0x03,
+            bytes([
+                0x00, 0x00,  # endereço inicial = 1
+                0x0B, 0x00    # quantidade = 11
+            ]))
+            if not isOk:
+                print("ERRO AO LER EMERGENCIA")
+                return False
+
+            tamanho, buffer = modbus_recebe_info(self.ser, False)
+            print("-="*25)
+            print("emergencia")
+            cont = 0
+            for h in bytes(buffer):
+                print(f"{cont}: {hex(h)}")
+                cont+=1
+
+            emergencia_ativa = buffer[4]
+            estrada = buffer[6]
+            direcao = buffer[8]
+            intersecao_id = buffer[10]
+            tipo_veiculo = buffer[12]
+            signal_group = buffer[14]
+            time_out = buffer[16]
+            nao_atendidos = buffer[18]
+            tempo_decorrido = buffer[20]
+            tempo_max = buffer[22]
+            modo = buffer[24]
+
+            
+            if emergencia_ativa == 0x01:
+                print("---"*25)
+                print("EMERGENCIA")
+                self.enviar_abrir(direcao)  
+
 
 # --- Exemplo de uso ---
 if __name__ == "__main__":
     servidor = Servidor_Central()
-    t = threading.Thread(
-                target=enviar_comandos_lentamente,
-                args=(servidor,),
-                daemon=True
-    )
-    t.start()
+    # t = threading.Thread(
+    #             target=enviar_comandos_lentamente,
+    #             args=(servidor,),
+    #             daemon=True
+    # )
+    # t.start()
 
     while True:
         pass
